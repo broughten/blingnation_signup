7cbb7a98e5932e8448f84f3238f8914dd3b18e6e
Jonathan Christensen <jc@kelsus.local>
2010-04-17 12:11:44 -0600

    email nudging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,10 @@ class ApplicationController < ActionController::Base
 
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  ssl_required
+  def ssl_required?
+    Rails.env.production?
+  end
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
